d783c1728a5b0faf7588fd1ed30ac5530c7a39d7
harishnagandla1 <harishnagandla1@gmail.com>
2024-08-30 19:11:10 +0530

    Create API/step_definitions/UserRegistrationSteps.java

---
+++ API/step_definitions/UserRegistrationSteps.java
@@ -0,0 +1,39 @@
+public class UserRegistrationSteps {
+
+    @Given("^the user has navigated to the registration page$")
+    public void the_user_has_navigated_to_the_registration_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the user enters valid registration details$")
+    public void the_user_enters_valid_registration_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the user submits the registration form$")
+    public void the_user_submits_the_registration_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the user should be registered successfully$")
+    public void the_user_should_be_registered_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the user enters invalid email$")
+    public void the_user_enters_invalid_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the user should be shown an error message$")
+    public void the_user_should_be_shown_an_error_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
