2186442525e96ad2457734388ba41a33b524f419
harishnagandla1 <harishnagandla1@gmail.com>
2024-08-30 19:11:40 +0530

    Create API/test_runners/UserRegistrationTestRunner.java

---
+++ API/test_runners/UserRegistrationTestRunner.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/java/resources/features",
+    glue = "stepdefinitions",
+    plugin = {"pretty", "html:target/cucumber-reports"},
+    monochrome = true
+)
+public class UserRegistrationTestRunner {}
